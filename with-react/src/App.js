a28b559d61781108db0652013fa45182f85bf8d0
dheerajj-07 <dheerajmk1369@gmail.com>
2025-10-04 17:29:23 +0530

    Adding navbar module

--- with-react/src/App.js
+++ with-react/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Navbar from './Components/Navbar';
 
 // App.js hai jo voh hmari main app hai aur jo bhi code hum isme likhenge voh main code hai jo app me dikhega
 
@@ -8,6 +9,7 @@ function App() {
   const[value, setValue] = useState(0);
   return (
     <div className="App">
+      <Navbar />
       <div className="value">{value}</div>
       <button onClick={()=>{
         setValue(value+1);
