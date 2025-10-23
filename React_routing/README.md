4e8d0a15285f0566c20895d17d58bd9b53656c40
dheerajj-07 <dheerajmk1369@gmail.com>
2025-10-23 16:12:19 +0530

    Routing

---
+++ React_routing/README.md
@@ -0,0 +1,10 @@
+Routing ki help se hum ek page se dusre page pe ja skte hai bina page reload kiye.. jab bhi hum redirect krenge toh bina reload huye page render hoga
+
+* npm i react-router-dom --> This command is used for install the react router DOM
+
+
+Page reload na ho isliye hum react router ka istemaal krte hain
+
+Hum anchor tag ki jgha Link tag use krenge, aur href ki jgha to use krenge
+<a href="/">old</a>
+<Link to="/">new</Link>
---
+++ React_routing/src/App.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' // create Browser router is recommended way to use
+import './App.css'
+import Login from './Components/Login'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import User from './Components/User'
+
+function App() {
+
+  const router =  createBrowserRouter([
+    {
+      path: "/",
+      element: <><Navbar/> <Home/></>
+    },
+    {
+      path: "/login",
+      element: <><Navbar/> <Login/></>
+    },
+    {
+      path: "/about",
+      element: <><Navbar/> <About/></>
+    },
+    {
+      path: "/User/:username",
+      element: <><Navbar/> <User/></>
+    }
+    
+  ])
+
+  return (
+    <>
+      
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
