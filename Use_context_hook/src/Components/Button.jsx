d1880bb528d061e49e7a757885c888b32da8b8f8
dheerajj-07 <dheerajmk1369@gmail.com>
2025-10-24 14:12:35 +0530

    change state in nested components

---
+++ Use_context_hook/src/Components/Button.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import Component1 from './Component1'
+import { counterContext } from '../Context/context'
+const Button = () => {
+    const value= useContext(counterContext)
+  return (
+    <div>
+        <button onClick={()=>{value.setCount((count)=>count+1)}}>
+            <span><Component1/></span>I am a Button
+        </button>
+    </div>
+  )
+}
+
+export default Button
