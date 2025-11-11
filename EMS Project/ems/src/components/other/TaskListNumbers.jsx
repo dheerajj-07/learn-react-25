a19608a31a2a1d127394363d8a7f52e060e849bf
dheerajj-07 <dheerajmk1369@gmail.com>
2025-11-11 18:09:16 +0530

    admin dashboard 1

--- EMS Project/ems/src/components/other/TaskListNumbers.jsx
+++ EMS Project/ems/src/components/other/TaskListNumbers.jsx
@@ -4,20 +4,20 @@ const TaskListNumbers = () => {
   return (
     <div className='flex box-border justify-between gap-5 mt-10'>
         <div className= 'rounded-xl w-[30%] px-9 py-6 bg-red-400'>
-            <h2 className='text-3xl font-semibold'>0</h2>
+            <h2 className='text-3xl font-semibold'>1</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
         <div className= 'rounded-xl w-[30%] px-9 py-6 bg-green-400'>
-            <h2 className='text-3xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h2 className='text-3xl font-semibold'>3</h2>
+            <h3 className='text-xl font-medium'>Completed Task</h3>
         </div>
         <div className= 'rounded-xl w-[30%] px-9 py-6 bg-blue-400'>
             <h2 className='text-3xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Accepted Task</h3>
         </div>
         <div className= 'rounded-xl w-[30%] px-9 py-6 bg-yellow-400'>
             <h2 className='text-3xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Failed Task</h3>
         </div>
     </div>
   )
